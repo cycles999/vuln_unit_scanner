df92d5810ad9b80640221cc34e0303b3785c2c73
cycles999 <132801338+cycles999@users.noreply.github.com>
2023-05-07 14:40:52 +0900

    bugfix

--- vuln_unit_scanner.sh
+++ vuln_unit_scanner.sh
@@ -5,7 +5,7 @@ files=$(find /etc/systemd/system)
 
 for file in $files;
 do
-        if [ -w $file ]
+        if [ -w $file ] && [ $(readlink $file) != "/dev/null" ]
         then
                 echo  $file
         fi
